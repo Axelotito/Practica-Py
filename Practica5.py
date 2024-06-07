25257e8ed577931e586752b4b28bd8f97ff5d1a7
Axelotito <axelinoxx1@gmail.com>
2024-06-07 14:08:26 -0600

    practica 5

---
+++ Practica5.py
@@ -0,0 +1,10 @@
+""" 
+ejercicio 5
+realiza una multiplicacion de dos numeros y muestra el resultado
+"""
+
+a = 3
+b = 4
+resultado = a * b
+
+print("La multiplicacion de los dos numero es:" , resultado)
