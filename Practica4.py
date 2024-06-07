b08746efaf5ef4aa0a7e263635629592de95c54d
Axelotito <axelinoxx1@gmail.com>
2024-06-07 13:56:04 -0600

    practica 4

---
+++ Practica4.py
@@ -0,0 +1,12 @@
+"""  
+Crea una lista con diferenctes 
+elementos e imprimirla
+
+"""
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for numeros in lista:
+    print(numeros)
+
+print("Tambien se podria ver así", lista)
