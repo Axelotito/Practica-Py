6d4a3b5527f6032e4a424f6d7de36f121517f2ec
Axelotito <axelinoxx1@gmail.com>
2024-06-07 14:18:03 -0600

    practica 6

---
+++ Practica6.py
@@ -0,0 +1,13 @@
+""" 
+Crear una cadena de texto y 
+muestra su longitud
+
+"""
+
+cad1 = "Hola Mundo"
+
+cantidad = len(cad1)
+
+print("La cadena es: \"", cad1, "\" y tiene de longitud:", cantidad, "palabras")
+
+""" Si quiero poner "" comillas en un string puedo ponerle \ estos y listo antes de poner la " """
