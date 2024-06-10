26823d48b45a47aec9b7fb772d986e97f51aeef5
Axelotito <axelinoxx1@gmail.com>
2024-06-09 20:44:08 -0600

    practica 21

---
+++ Practica21.py
@@ -0,0 +1,15 @@
+""" Multiplica una cadena por un numero entero """
+
+cadena = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for i in range(len(cadena)):
+    cadena[i] = cadena[i] * 2
+
+print(cadena)
+
+""" no se referia a eso XD """
+
+cadena = 'hola '
+multi = cadena * 10
+
+print(multi)
