13afbb73d192373f34b894a59b2580637a7c4518
Axelotito <axelinoxx1@gmail.com>
2024-06-21 22:00:20 -0600

    Agrego carpeta Codedex con archivo Hypotenuse.py

---
+++ Codedex/Hypotenuse.py
@@ -0,0 +1,5 @@
+ladoA = input("Ingrese el valor del lado A: ")
+ladoB = input("Ingrese el valor del lado B: ")
+hipotenusa = (int(ladoA)**2 + int(ladoB)**2)**0.5
+
+print("La hipotenusa es:", hipotenusa)
