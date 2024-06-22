a8b48a9b5289cf3727fd742af1e4af09a5fe881e
Axelotito <axelinoxx1@gmail.com>
2024-06-21 22:01:31 -0600

    modified:   Codedex/Hypotenuse.py

--- Codedex/Hypotenuse.py
+++ Codedex/Hypotenuse.py
@@ -1,3 +1,5 @@
+""" Esto es el ejercicio 9 de codedex de la pagina """
+
 ladoA = input("Ingrese el valor del lado A: ")
 ladoB = input("Ingrese el valor del lado B: ")
 hipotenusa = (int(ladoA)**2 + int(ladoB)**2)**0.5
