850dbbdd53133a96e08663b4d45716a75465638c
Axelotito <axelinoxx1@gmail.com>
2024-07-01 19:56:14 -0600

    add detention file

---
+++ Codedex/detention.py
@@ -0,0 +1,10 @@
+
+for i in range(100):
+    print('I will not use Snapchat in class')
+    print(f'Iteration {i+1}')
+
+    student_name = input('Enter student name: ')
+    screen_name = input('Enter screen name: ')
+    print(f'Student: {student_name}')
+    print(f'Screen: {screen_name}')
+
