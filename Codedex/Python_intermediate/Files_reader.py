4d6a47311406fd0279ea328c9185730b3ea723c2
Luis Axel <axel.cruz.gr@gmail.com>
2024-07-13 23:10:14 -0600

    modified: Files_reader.py and Example_file_reader.txt

--- Codedex/Python_intermediate/Files_reader.py
+++ Codedex/Python_intermediate/Files_reader.py
@@ -3,3 +3,15 @@ file = open('example.txt', 'w')
 file.write('Hello World\n')
 file.write('This is our new text file\n')
 file.write('Hola que tal\n')
+
+file2 = open('.\Codedex\Python_intermediate\Example_file_reader.txt', 'w')
+
+lines = [ 'hola esto', '\nes una', '\nlinea nueva']
+file2.writelines(lines)
+file2.close()
+
+file2 = open('.\Codedex\Python_intermediate\Example_file_reader.txt', 'r')
+print('using read() method: ')
+content = file2.read()
+print(content)
+file2.close()
