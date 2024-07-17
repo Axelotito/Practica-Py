2a47b27cb3f6c2d0499f3e18deb997a9887c32dd
Luis Axel <axel.cruz.gr@gmail.com>
2024-07-16 21:39:21 -0600

    feat: Add functionality to read and write packing list from CSV file
    This commit adds functionality to read and write the packing list from a CSV file. If the file exists, it is read and its contents are printed. If the file does not exist, a new file is created and the packing list data is written to it.

--- Codedex/Python_intermediate/10_Packing_List.py
+++ Codedex/Python_intermediate/10_Packing_List.py
@@ -8,5 +8,12 @@ data = [
 ]
 
 try: 
-    with open('packing_list.csv', 'r', encoding= 'utf-8') as file: 
-        
+    with open('D:\Programacion\Python\Practica\Codedex\Python_intermediate\packing_list.csv', 'r', encoding= 'utf-8') as file: 
+        archivo = csv.reader(file)
+        for filas in archivo:
+            print(filas)
+except FileNotFoundError: 
+    print("Packing list file not found. Creating a new one.")
+    with open('D:\Programacion\Python\Practica\Codedex\Python_intermediate\packing_list.csv', 'w', encoding= 'utf-8') as file:
+        archivo = csv.writer(file)
+        archivo.writerows(data)
