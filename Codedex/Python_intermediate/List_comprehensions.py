471a00dbb52bc533ba09b976d954b94e7541a84e
Luis Axel <axel.cruz.gr@gmail.com>
2024-07-31 22:59:46 -0600

    feat: Update HTML files with new content
    This commit adds new content to the `blueprint.html` and `family_tree.html` files. The `blueprint.html` file now includes a title, heading, paragraph, and body tags. The `family_tree.html` file includes a heading, list of family members, and body tags. These changes are made to improve the structure and readability of the HTML files.

---
+++ Codedex/Python_intermediate/List_comprehensions.py
@@ -0,0 +1,25 @@
+# Write code below 💖
+
+numbers_1 = [ 1,2,3,4,5]
+squares = []
+
+for num in numbers_1:
+  squares.append(num**2)
+
+''' there is another way to make this '''
+
+squared_numbers = [num ** 2 for num in numbers_1]
+
+print('original numbers:', numbers_1)
+print('Squared numbers:', squared_numbers)
+print('Squared numbers:',squares)
+
+''' excercise '''
+numbers_2 = [57, 10, 82, 36, 89, 46, 13, 23, 92, 48]
+
+def numbers_even(number):
+  return (number % 2) == 0
+ 
+even_numbers = [ num for num in numbers_2 if num % 2 == 0 ]
+
+print('Even numbers:', even_numbers)
