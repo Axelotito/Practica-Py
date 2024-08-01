7f55a5d975d705960f61ccba8d2b8d6a3426f001
Luis Axel <axel.cruz.gr@gmail.com>
2024-07-31 23:18:34 -0600

    ```text refactor: Update list comprehensions in List_comprehensions.py
    This commit refactors the list comprehensions in the `List_comprehensions.py` file. It improves the readability and consistency of the code by removing unnecessary whitespace and using consistent indentation. The changes also include updating the comments to use triple double quotes instead of triple single quotes for consistency.

--- Codedex/Python_intermediate/List_comprehensions.py
+++ Codedex/Python_intermediate/List_comprehensions.py
@@ -1,25 +1,27 @@
 # Write code below 💖
 
-numbers_1 = [ 1,2,3,4,5]
+numbers_1 = [1, 2, 3, 4, 5]
 squares = []
 
 for num in numbers_1:
-  squares.append(num**2)
+    squares.append(num**2)
 
-''' there is another way to make this '''
+""" there is another way to make this """
 
-squared_numbers = [num ** 2 for num in numbers_1]
+squared_numbers = [num**2 for num in numbers_1]
 
-print('original numbers:', numbers_1)
-print('Squared numbers:', squared_numbers)
-print('Squared numbers:',squares)
+print("original numbers:", numbers_1)
+print("Squared numbers:", squared_numbers)
+print("Squared numbers:", squares)
 
-''' excercise '''
+""" excercise """
 numbers_2 = [57, 10, 82, 36, 89, 46, 13, 23, 92, 48]
 
-def numbers_even(number):
-  return (number % 2) == 0
- 
-even_numbers = [ num for num in numbers_2 if num % 2 == 0 ]
 
-print('Even numbers:', even_numbers)
+def numbers_even(number):
+    return (number % 2) == 0
+
+
+even_numbers = [num for num in numbers_2 if num % 2 == 0]
+
+print("Even numbers:", even_numbers)
