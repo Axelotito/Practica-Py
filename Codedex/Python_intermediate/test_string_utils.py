c36e0c16212956c6ec38e18967a192dc87d0ca66
Luis Axel <axel.cruz.gr@gmail.com>
2024-08-09 21:38:47 -0600

    feat: Add unit tests for reverse_string function

--- Codedex/Python_intermediate/test_string_utils.py
+++ Codedex/Python_intermediate/test_string_utils.py
@@ -1,6 +1,9 @@
 import string_utils
 import unittest
 
+""" para hacer una prueba unitaria necesitas importar 
+unittest y crear una clase que herede de unittest.TestCase"""
+
 class TestStringUtils(unittest.TestCase):
     def test_reverse_string(self):
         result = string_utils.reverse_string('hello')
@@ -8,10 +11,13 @@ class TestStringUtils(unittest.TestCase):
         self.assertTrue(result in 'olleh')	
 
     def test_capitalized_string(self):
-        result = self.string_utils.capitalize_string('hello')
+        result = string_utils.capitalize_string('hello')
         self.assertEqual(result, 'Hello')
         self.assertTrue(result[0] in 'H')
 
+    def test_is_capitalized(self):
+        result = string_utils.is_capitalized('Hello')
+        self.assertTrue(result, True)
 
 if __name__ == '__main__':
     unittest.main()
