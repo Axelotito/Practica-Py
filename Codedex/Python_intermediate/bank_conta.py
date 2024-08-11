847f24c7935ec268c34b8d641037c1a942bfe788
Luis Axel <axel.cruz.gr@gmail.com>
2024-08-10 23:22:28 -0600

    Add test_bank_cuenta.py file

--- Codedex/Python_intermediate/bank_conta.py
+++ Codedex/Python_intermediate/bank_conta.py
@@ -1,2 +1,19 @@
 class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.balance = initial_balance
+
+    def deposit(self, amount):
+        if amount < 0:
+            #raise regresa un mensaje de error si el valor es negativo 
+            raise ValueError('Withdrawal amount must be positive')
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <=0:
+            raise ValueError('Withdrawal amount must be positive')
+        elif amount > self.balance:
+            raise ValueError('Insufficient funds')
+        self.balance -= amount
     
+cuenta = BankAccount(100)
+ 
