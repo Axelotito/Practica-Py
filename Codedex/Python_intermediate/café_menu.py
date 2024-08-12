9dbc23e08349d1e4fd7fdde07c8f23bd6f00174c
Luis Axel <axel.cruz.gr@gmail.com>
2024-08-11 19:46:26 -0600

    feat: Add CoffeeMenu class with menu dictionary
    The code changes in the `caf\303\251_menu.py` file add a new class `CoffeeMenu` with a `menu` dictionary. This dictionary contains the names of different coffee types as keys and their corresponding prices as values. This change enhances the functionality of the code by providing a centralized menu for coffee options.

---
+++ Codedex/Python_intermediate/café_menu.py
@@ -0,0 +1,10 @@
+class CoffeeMenu:
+    menu = {}
+
+    def __init__(self):
+        self.menu = {
+            "espresso": 2.50,
+            "latte": 2.75,
+            "cappuccino": 3.20,
+            "americano": 2.70,
+        }
