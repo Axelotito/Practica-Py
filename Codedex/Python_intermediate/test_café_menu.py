cece24ffc26b6ca3d12d109a8d9cd1bcf75ca86a
Luis Axel <axel.cruz.gr@gmail.com>
2024-08-11 20:02:03 -0600

    feat: Add test cases for get_price_existing_item and get_price_non_existing_item methods
    The code changes in the `test_café_menu.py` file add test cases for the `get_price_existing_item` and `get_price_non_existing_item` methods of the `CoffeeMenu` class. These test cases ensure that the `get_price` method returns the correct price for an existing coffee item and `None` for a non-existing item. This change improves the reliability and accuracy of the code.

--- Codedex/Python_intermediate/test_café_menu.py
+++ Codedex/Python_intermediate/test_café_menu.py
@@ -2,12 +2,13 @@ import unittest
 from café_menu import CoffeeMenu
 
 class TestCoffeeMenu(unittest.TestCase):
-    def test_get_price_existing_item():
+    def test_get_price_existing_item(self):
         menu = CoffeeMenu.get_price("espresso")
+        self.assetEqual(menu, 2.50)
 
-    def test_get_price_non_existing_item():
+    def test_get_price_non_existing_item(self):
         menu = CoffeeMenu.get_price('Pokemocha')
-
+        self.assetEqual(menu, None)
 
     def test_add_item(self):
         menu = CoffeeMenu.add_item("mocha", 3.50)
